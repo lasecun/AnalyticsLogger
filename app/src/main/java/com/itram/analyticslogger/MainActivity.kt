1a3080d005afb7edf5c7f40ec8827fa587c6397b
Juanjo Marti <juanjo.marti@colba.es>
2022-11-26 18:15:35 +0100

    Add Delegate

--- app/src/main/java/com/itram/analyticslogger/MainActivity.kt
+++ app/src/main/java/com/itram/analyticslogger/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.itram.analyticslogger
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    AnalyticsLogger by AnalyticsLoggerImpl(),
+    DeepLinkHandler by DeepLinkHandlerImpl() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        registerLifeCycleOwner(this)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        handleDeepLink(this, intent)
+    }
+}
+
+interface DeepLinkHandler {
+    fun handleDeepLink(activity: AppCompatActivity, intent: Intent?)
+}
+
+class DeepLinkHandlerImpl : DeepLinkHandler {
+    override fun handleDeepLink(activity: AppCompatActivity, intent: Intent?) {
+
+    }
+
+}
+
+interface AnalyticsLogger {
+    fun registerLifeCycleOwner(owner: LifecycleOwner)
+}
+
+class AnalyticsLoggerImpl : AnalyticsLogger, LifecycleEventObserver {
+    override fun registerLifeCycleOwner(owner: LifecycleOwner) {
+        owner.lifecycle.addObserver(this)
+    }
+
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        when (event) {
+            Lifecycle.Event.ON_RESUME -> println("User opened the screen")
+            Lifecycle.Event.ON_PAUSE -> println("User opened the screen")
+            else -> Unit
+        }
     }
 }
